1b2a0c590d1019be1ed614e222bdac74f9641e70
DwayneDevelops <DwayneDevelops@gmail.com>
2019-06-17 17:37:03 -0500

    inital commit

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,22 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require('./enhancer.js');
 // test away!
+
+describe ('enhancer.js', () => {
+    describe('succeed()', () => {
+        it()
+    })
+    
+
+    describe('repair()', () => {
+        it('restores durability to 100', () => {
+            expect(repair({ durability: 89 }).durability).toBe(100);
+            expect(repair({ durability: -89 }).durability).toBe(100);
+            expect(repair({ durability: 100 }).durability).toBe(100);
+            expect(repair({ durability: 0 }).durability).toEqual(100);
+        });
+    });
+
+    describe('success()', () => {});
+
+    it.todo('should have a max of 100 for durability');
+});
